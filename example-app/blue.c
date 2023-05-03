88ff7b26abbce1b20116fee6c2cfeba4b4232f71
Michael Cardell Widerkrantz <mc@tillitis.se>
2023-05-03 17:58:15 +0200

    Add simple example app

---
+++ example-app/blue.c
@@ -0,0 +1,26 @@
+#include <types.h>
+#include <tk1_mem.h>
+
+#define SLEEPTIME 100000
+#define LED_RED   (1 << TK1_MMIO_TK1_LED_R_BIT)
+#define LED_GREEN (1 << TK1_MMIO_TK1_LED_G_BIT)
+#define LED_BLUE  (1 << TK1_MMIO_TK1_LED_B_BIT)
+
+static volatile uint32_t *led = (volatile uint32_t *)TK1_MMIO_TK1_LED;
+
+void sleep(uint32_t n)
+{
+	for (volatile int i = 0; i < n; i++);
+}
+
+int main(void)
+{
+	for (;;) {
+		*led = LED_RED;
+		sleep(SLEEPTIME);
+		*led = LED_GREEN;
+		sleep(SLEEPTIME);
+		*led = LED_BLUE;
+		sleep(SLEEPTIME);
+	}
+}

42d3c14a0fd82c4ab37fb9cab0e9adaa25b70b59
Michael Cardell Widerkrantz <mc@tillitis.se>
2023-06-27 17:10:02 +0200

    New type: uint16_t

--- include/types.h
+++ include/types.h
@@ -12,6 +12,7 @@ typedef long long int64_t;
 typedef unsigned char uint8_t;
 typedef char int8_t;
 typedef signed short int int16_t;
+typedef unsigned short int uint16_t;
 typedef unsigned long size_t;
 
 #define NULL ((char *)0)

a37b3f1fb9258d56e8d8f96080ddab47f85f1cc3
Michael Cardell Widerkrantz <mc@tillitis.se>
2024-01-29 15:26:34 +0100

    Use tkey/led in example app

--- example-app/blue.c
+++ example-app/blue.c
@@ -2,15 +2,11 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <stdint.h>
+#include <tkey/led.h>
 #include <tkey/tk1_mem.h>
 #include <tkey/qemu_debug.h>
 
 #define SLEEPTIME 100000
-#define LED_RED   (1 << TK1_MMIO_TK1_LED_R_BIT)
-#define LED_GREEN (1 << TK1_MMIO_TK1_LED_G_BIT)
-#define LED_BLUE  (1 << TK1_MMIO_TK1_LED_B_BIT)
-
-static volatile uint32_t* const led = (volatile uint32_t *)TK1_MMIO_TK1_LED;
 
 void sleep(uint32_t n)
 {
@@ -25,11 +21,11 @@ int main(void)
 	qemu_lf();
 
 	for (;;) {
-		*led = LED_RED;
+		led_set(LED_RED);
 		sleep(SLEEPTIME);
-		*led = LED_GREEN;
+		led_set(LED_GREEN);
 		sleep(SLEEPTIME);
-		*led = LED_BLUE;
+		led_set(LED_BLUE);
 		sleep(SLEEPTIME);
 	}
 }

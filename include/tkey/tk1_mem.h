79b9ee71c19a0f3286b762771d7ef43bf0c013d7
Michael Cardell Widerkrantz <mc@tillitis.se>
2025-03-07 15:03:29 +0100

    Synchronize tk1_mem.h with upstream
    tk1_mem.h usually lives in tillitis-key1 and has been updated there.
    Synchronize with tkey-libs. From now tk1_mem.h will now live in
    tkey-libs and tkey-libs will be used in tillitis-key1.
    
    - Increase ROM_SIZE.
    - Increase FW_RAM_SIZE.
    - Correct UDS address.
    - Remove SWITCH_APP, SYSTEM_MODE_CTRL, and BLAKE2S.

--- include/tkey/tk1_mem.h
+++ include/tkey/tk1_mem.h
@@ -4,6 +4,9 @@
  * SPDX-FileCopyrightText: 2022 Tillitis AB <tillitis.se>
  * SPDX-License-Identifier: BSD-2-Clause
  *
+ * Note that this file is also in tillitis-key1 and qemu
+ * (GPL-2.0-or-later). Needs to stay in sync and have a compatible
+ * license.
  */
 
 // clang-format off
@@ -12,15 +15,17 @@
 #define TKEY_TK1_MEM_H
 
 /*
-
   The canonical location of this file is in:
 
-  https://github.com/tillitis/tillitis-key1
+    https://github.com/tillitis/tkey-libs
 
-  /hw/application_fpga/fw/tk1_mem.h
+  Under:
 
-  The contents are derived from the Verilog code. For use by QEMU model,
-  firmware, and apps.
+    include/tkey/tk1_mem.h
+
+  The contents are mostly derived from the Verilog code in
+
+    https://github.com/tillitis/tillitis-key1
 
   Memory map
 
@@ -31,7 +36,7 @@
   ROM		0b00	30 bit address
   RAM		0b01	30 bit address
   Reserved	0b10
-  MMIO		0b11	6 bits for core select, 24 bits rest
+  Cores		0b11	6 bits for core select, 24 bits rest
 
   Address Prefix, the first 8 bits in a 32-bit address:
 
@@ -39,18 +44,19 @@
   --------------------
   ROM		0x00
   RAM		0x40
-  MMIO		0xc0
-  MMIO TRNG	0xc0
-  MMIO TIMER	0xc1
-  MMIO UDS	0xc2
-  MMIO UART	0xc3
-  MMIO TOUCH	0xc4
-  MMIO FW_RAM	0xd0
-  MMIO QEMU	0xfe   Not used in real hardware
-  MMIO TK1	0xff
+  TRNG		0xc0
+  TIMER		0xc1
+  UDS		0xc2
+  UART		0xc3
+  TOUCH		0xc4
+  FW_RAM	0xd0
+  QEMU		0xfe   Not used in real hardware
+  TK1		0xff
  */
 
 #define TK1_ROM_BASE 0x00000000
+#define TK1_ROM_SIZE 0x2000
+
 #define TK1_RAM_BASE 0x40000000
 #define TK1_RAM_SIZE 0x20000
 
@@ -60,8 +66,8 @@
 #define TK1_APP_MAX_SIZE 0x20000
 
 #define TK1_MMIO_FW_RAM_BASE 0xd0000000
-// FW_RAM is 2048 bytes
-#define TK1_MMIO_FW_RAM_SIZE 0x800
+// FW_RAM is 4096 bytes
+#define TK1_MMIO_FW_RAM_SIZE 0x1000
 
 #define TK1_MMIO_TRNG_BASE 0xc0000000
 #define TK1_MMIO_TRNG_STATUS 0xc0000024
@@ -79,8 +85,8 @@
 #define TK1_MMIO_TIMER_TIMER 0xc100002c
 
 #define TK1_MMIO_UDS_BASE 0xc2000000
-#define TK1_MMIO_UDS_FIRST 0xc2000040
-#define TK1_MMIO_UDS_LAST 0xc200005c
+#define TK1_MMIO_UDS_FIRST 0xc2000000
+#define TK1_MMIO_UDS_LAST 0xc200001c
 
 #define TK1_MMIO_UART_BASE 0xc3000000
 #define TK1_MMIO_UART_RX_STATUS 0xc3000080
@@ -103,10 +109,6 @@
 #define TK1_MMIO_TK1_NAME1 0xff000004
 #define TK1_MMIO_TK1_VERSION 0xff000008
 
-// Deprecated - use _SYSTEM_MODE_CTRL instead
-#define TK1_MMIO_TK1_SWITCH_APP 0xff000020
-#define TK1_MMIO_TK1_SYSTEM_MODE_CTRL 0xff000020
-
 #define TK1_MMIO_TK1_LED 0xff000024
 #define TK1_MMIO_TK1_LED_R_BIT 2
 #define TK1_MMIO_TK1_LED_G_BIT 1
@@ -120,8 +122,6 @@
 
 #define TK1_MMIO_TK1_APP_ADDR 0xff000030
 #define TK1_MMIO_TK1_APP_SIZE 0xff000034
-
-#define TK1_MMIO_TK1_BLAKE2S 0xff000040
 
 #define TK1_MMIO_TK1_CDI_FIRST 0xff000080
 #define TK1_MMIO_TK1_CDI_LAST 0xff00009c

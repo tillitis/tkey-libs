7c47c96662cd4d8df3119fd3896c5734da4f3927
Michael Cardell Widerkrantz <mc@tillitis.se>
2025-03-07 15:31:27 +0100

    Make blake2s not implemented
    We just removed the blake2s memory access to the firmware so this
    function is currently not implemented. We will introduce a BLAKE2s in
    the library soon.

--- libcommon/blake2s.c
+++ libcommon/blake2s.c
@@ -5,16 +5,10 @@
 #include <tkey/blake2s.h>
 #include <tkey/tk1_mem.h>
 
-typedef int (*fw_blake2s_p)(void *out, unsigned long outlen, const void *key,
-			    unsigned long keylen, const void *in,
-			    unsigned long inlen, blake2s_ctx *ctx);
-
 int blake2s(void *out, unsigned long outlen, const void *key,
 	    unsigned long keylen, const void *in, unsigned long inlen,
 	    blake2s_ctx *ctx)
 {
-	fw_blake2s_p const fw_blake2s =
-	    (fw_blake2s_p) * (volatile uint32_t *)TK1_MMIO_TK1_BLAKE2S;
-
-	return fw_blake2s(out, outlen, key, keylen, in, inlen, ctx);
+	// Not implemented.
+	return -1;
 }

b26af26330b009e22404171335d254d79cd52b16
Mikael Ågren <mikael@tillitis.se>
2025-10-03 15:22:05 +0200

    Support platform identification
    Adds version numbers for Bellatrix and Castor. This number can be
    compared with the value in TK1_MMIO_TK1_VERSION to detect which platform
    we're running on.
    
    Co-authored-by: Michael Cardell Widerkrantz <mc@tillitis.se>

---
+++ include/tkey/platform.h
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Tillitis AB <tillitis.se>
+// SPDX-License-Identifier: BSD-2-Clause
+#ifndef TKEY_PLATFORM_H
+#define TKEY_PLATFORM_H
+
+// Platform identification
+#define TKEY_VERSION_BELLATRIX 5
+#define TKEY_VERSION_CASTOR 6
+
+#endif

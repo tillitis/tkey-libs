791f5e8c05f45d68d27e0838e539ec8b83b30cd9
Niels Möller <nisse@glasklarteknik.se>
2025-10-27 11:25:01 +0100

    Add string.h header file for standard C string functions
    Add corresponding source file libcommon/string.c. The functions
    strlen, memset, memcpy are moved from lib.h/lib.c, and updated to more
    standard prototypes (with size_t rather than unsigned for size).
    
    New functions strcmp, memcmp, memchr.

---
+++ include/string.h
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2022 Tillitis AB <tillitis.se>
+// SPDX-FileCopyrightText: 2025 Glasklar Teknik AB <glasklarteknik.se>
+// SPDX-License-Identifier: BSD-2-Clause
+
+#ifndef TKEY_STRING_H
+#define TKEY_STRING_H
+
+#include <stddef.h>
+
+size_t strlen(const char *str);
+int strcmp(const char *a, const char *b);
+void *memset(void *dest, int c, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
+int memcmp(const void *a, const void *b, size_t n);
+void *memchr(const void *p, int c, size_t n);
+
+#endif /* TKEY_STRING_H */

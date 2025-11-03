bd330127c6f12d52d007aac527163f5f3c4e4007
Niels Möller <nisse@glasklarteknik.se>
2025-11-03 15:45:28 +0100

    New string.h functions strcmp, memcmp, and memchr

--- include/string.h
+++ include/string.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2022 Tillitis AB <tillitis.se>
+// SPDX-FileCopyrightText: 2025 Glasklar Teknik AB <glasklarteknik.se>
 // SPDX-License-Identifier: BSD-2-Clause
 
 #ifndef TKEY_STRING_H
@@ -7,7 +8,10 @@
 #include <stddef.h>
 
 size_t strlen(const char *str);
+int strcmp(const char *a, const char *b);
 void *memset(void *dest, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
+int memcmp(const void *a, const void *b, size_t n);
+void *memchr(const void *p, int c, size_t n);
 
 #endif /* TKEY_STRING_H */

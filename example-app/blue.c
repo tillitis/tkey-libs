7d3a4a0c41b2e5c511a95279e8c5e42d4614f4ff
Daniel Jobson <jobson@tillitis.se>
2025-12-16 14:50:44 +0100

    testing non-blocking

--- example-app/blue.c
+++ example-app/blue.c
@@ -2,15 +2,18 @@
 // SPDX-License-Identifier: BSD-2-Clause
 
 #include <stdint.h>
+#include <tkey/assert.h>
+#include <tkey/debug.h>
+#include <tkey/io.h>
 #include <tkey/led.h>
 #include <tkey/tk1_mem.h>
-#include <tkey/debug.h>
 
 #define SLEEPTIME 100000
 
 void sleep(uint32_t n)
 {
-	for (volatile int i = 0; i < n; i++);
+	for (volatile int i = 0; i < n; i++)
+		;
 }
 
 int main(void)
@@ -20,7 +23,20 @@ int main(void)
 	debug_putinthex(SLEEPTIME);
 	debug_lf();
 
+	uint8_t buf[128] = {0x00};
+	enum ioend endpoint = IO_NONE;
+	uint8_t len;
+
 	for (;;) {
+		if (readselect(IO_CDC, true, &endpoint, &len)) {
+			assert(1 == 2);
+		}
+
+		if (len > 0) {
+			int nbytes = read(IO_CDC, buf, 128, len);
+			write(IO_CDC, buf, nbytes);
+		}
+
 		led_set(LED_RED);
 		sleep(SLEEPTIME);
 		led_set(LED_GREEN);
